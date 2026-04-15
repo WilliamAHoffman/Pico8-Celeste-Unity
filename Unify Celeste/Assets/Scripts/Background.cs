3e0b0c7cf91b3cfeb79b74f4a0984a74f5241c36
lsiverts <Lilysivertson05@gmail.com>
2026-04-14 23:23:30 -0400

    Fading Blocks fade, background spawns with clouds
    the background spawns in now with clouds, the fading blocks animate and fade when you step on them

--- Unify Celeste/Assets/Scripts/Background.cs
+++ Unify Celeste/Assets/Scripts/Background.cs
@@ -7,11 +7,21 @@ public class Background : MonoBehaviour
     [SerializeField] Color bg_color;
     [SerializeField] Color cloud_color;
     [SerializeField] GameObject cloud_prefab;
+    
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        for (int i=0; i<15; i++)
+        {
+            float minY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+            float maxY = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+            float minX = Camera.main.ViewportToWorldPoint(new Vector3(-.5f, 0, 0)).x;
+            float maxX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+            Instantiate(cloud_prefab, new Vector3(Random.Range(minX,maxX), Random.Range(minY, maxY), 0), Quaternion.identity);
+
+        }
         InvokeRepeating("SpawnCloud", .1f, .1f);
     }
 
--- Unify Celeste/Assets/Scripts/FadingBlock.cs
+++ Unify Celeste/Assets/Scripts/FadingBlock.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class FadingBlock : MonoBehaviour
 {
+
+    Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim= GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.Play("FadingBlockAnim");
+    }
+
+    public void Fade()
+    {
+        gameObject.SetActive(false);
+        Invoke("ResetBlock", 3);
+    }
+
+    private void ResetBlock()
+    {
+        
+        gameObject.SetActive(true);
+        anim.Play("Idle");
     }
 }
