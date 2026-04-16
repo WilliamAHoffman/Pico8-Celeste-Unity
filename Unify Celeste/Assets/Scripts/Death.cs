a8bf2c538410ffba07d63330f5ae73addbde0028
cjbytheocean <cjrodenbush2024@gmail.com>
2026-04-16 13:11:07 -0400

    Erased incorrect death implementation.

--- Unify Celeste/Assets/Scripts/Death.cs
+++ Unify Celeste/Assets/Scripts/Death.cs
@@ -4,16 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D c)
-    {
-        if (c.CompareTag("Spikes"))
-        {
-            StartCoroutine(ReloadScene());
-            Debug.Log("routine started");
-            gameObject.SetActive(false); // this will need to be changed
-        } 
-    }
-
     IEnumerator ReloadScene()
     {
         yield return new WaitForSeconds(2f);
