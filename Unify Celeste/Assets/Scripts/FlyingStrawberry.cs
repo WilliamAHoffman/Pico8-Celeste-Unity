8215432bb76f2ab4fe671a6dd0ca7d93a77f693b
cjbytheocean <cjrodenbush2024@gmail.com>
2026-04-22 18:00:46 -0400

    Summit

---
+++ Unify Celeste/Assets/Scripts/FlyingStrawberry.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FlyingStrawberry : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Collider2D cl;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        cl = GetComponent<Collider2D>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.GetComponent<PlayerController>())
+        {
+            sr.enabled = false;
+            cl.enabled = false;
+            GameManager.instance.strawberryCounter++;
+        }
+
+        
+    }
+    
+}
