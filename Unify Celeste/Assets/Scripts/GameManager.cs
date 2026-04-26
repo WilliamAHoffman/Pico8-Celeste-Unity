30003fd4d561871888000b7ddaece5356628afaa
WilliamAHoffman <whoffman4230@gmail.com>
2026-04-26 16:07:15 -0400

    added left right check

--- Unify Celeste/Assets/Scripts/GameManager.cs
+++ Unify Celeste/Assets/Scripts/GameManager.cs
@@ -66,6 +66,7 @@ public class GameManager : MonoBehaviour
         gameActive = false;
 
         if(!player) player = Instantiate(playerPrefab, new Vector3(0,0,0), Quaternion.identity);
+        player.GetComponent<PlayerController>().faceRight = true;
         AllowMovement(false);
         player.transform.position = new Vector3(spawnLocation.x,-9);
         playerMoveUp = true;
